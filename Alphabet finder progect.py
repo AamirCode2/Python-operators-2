5fdb82864bcd7223e27a43857b92ff2d43b309ae
AamirCode2 <aamirlovesart@gmail.com>
2025-07-25 19:50:49 +1000

    Add files via upload

---
+++ Alphabet finder progect.py
@@ -0,0 +1,6 @@
+Character = str(input("Enter a character to find out if it is part of the alphabet: "))
+
+if ('a' <= Character) and ('z' >= Character):
+    print("Your character is part of the alphabet")
+else:
+    print("Your character is not part of the alphabet")
